276b70957158bccb8f84abba8c2a3aa4e15cbfd8
Yeswanth Reddy <yeswanth.reddy@almonds.ai>
2025-09-11 16:54:33 +0530

    fixes

--- src/components/TeacherExams.js
+++ src/components/TeacherExams.js
@@ -353,9 +353,9 @@ const TeacherExams = () => {
   };
 
   return (
-    <div className="flex h-screen">
+    <div className="min-h-screen">
       <TeacherSidebar />
-      <div className="flex-1 flex flex-col">
+      <div className="ml-80 flex flex-col min-h-screen">
         <Header />
         <div className="flex-1 overflow-auto" style={{backgroundColor: '#f0f2f5'}}>
           <div className="p-6">
